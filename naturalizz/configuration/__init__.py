78b31c170b75e73606da86e0742e8bc45bf69f4d
Gotyee <86921217+Gotyee@users.noreply.github.com>
2024-05-21 20:58:31 +0200

    added pyrenean list

--- naturalizz/configuration/__init__.py
+++ naturalizz/configuration/__init__.py
@@ -2,6 +2,7 @@ from .constants import NB_PIC_DISPLAYED, RANKS, TAXON_TYPE
 from .dataframe_generator import generate_df_from_taxon_config
 from .insect_taxon import INSECT_TO_SEARCH
 from .plant_taxon import PLANTS_FAMILIES, PLANTS_SPECIES_TO_SEARCH
+from .pyrennean_taxon import PYRENEAN_FAMILIES_TO_SEARCH, PYRENEAN_SPECIES_TO_SEARCH
 
 __all__ = [
     "INSECT_TO_SEARCH",
@@ -11,4 +12,6 @@ __all__ = [
     "PLANTS_FAMILIES",
     "generate_df_from_taxon_config",
     "TAXON_TYPE",
+    "PYRENEAN_FAMILIES_TO_SEARCH",
+    "PYRENEAN_SPECIES_TO_SEARCH",
 ]
